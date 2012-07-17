193095d66591898db2f9b24b6e687ce887c5e550
yrtimiD <yrtimiD@gmail.com>
2012-07-18 00:39:43 +0300

    geofabrik.de list update

--- scripts/geofabrik.de/create_pbf_list.sh
+++ scripts/geofabrik.de/create_pbf_list.sh
@@ -1,4 +1,5 @@
 wget -r -L -e robots=off -A html http://download.geofabrik.de/osm/
 grep -r "osm.pbf" *|sort >rows
 perl -n -e'/([^:]+)index.+a href="([^"]+)/ && print "http://$1$2\n"' rows >pbf_list
+rm rows
 
