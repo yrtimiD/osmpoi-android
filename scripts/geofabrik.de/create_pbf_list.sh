16c68db6c4ef0e39786dbd1cf84433be77604731
yrtimiD <yrtimiD@gmail.com>
2012-10-09 23:15:52 +0200

    updated file list from geofabrik

--- scripts/geofabrik.de/create_pbf_list.sh
+++ scripts/geofabrik.de/create_pbf_list.sh
@@ -1,5 +1,6 @@
-wget -r -L -e robots=off -A html http://download.geofabrik.de/osm/
+wget -r -L -e robots=off -A html http://download.geofabrik.de/openstreetmap/
 grep -r "osm.pbf" *|sort >rows
 perl -n -e'/([^:]+)index.+a href="([^"]+)/ && print "http://$1$2\n"' rows >pbf_list
 rm rows
+rm -rf download.geofabrik.de
 
